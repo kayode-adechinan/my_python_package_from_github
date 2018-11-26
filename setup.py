7a39d837616a37be1e6bb38fdb8c250ef3c4b1bf
kayode-adechinan <kayode.adechinan@gmail.com>
2018-11-26 15:51:45 -0500

    ddd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="my_python_package_from_github",
-    version="0.0.1",
+    version="0.0.2",
     author="Kayode Adechinan",
     author_email="kayode.adechinan@gmail.com",
     description="simple python package hosted on Github",
